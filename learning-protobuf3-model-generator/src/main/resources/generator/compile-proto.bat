30bf5080b3eb5cd8612aa484b4326edec2a36bcd
letsprog <farah.fertassi.dev@gmail.com>
2016-09-29 01:15:41 +0100

    Minor Fix : Better Arg Value

--- learning-protobuf3-model-generator/src/main/resources/generator/compile-proto.bat
+++ learning-protobuf3-model-generator/src/main/resources/generator/compile-proto.bat
@@ -1,5 +1,5 @@
 
-set PROTOBUF_BIN_DIR=%1
+set PROTOBUF_DIR=%1
 set SRC_DIR=%2
 set PROTO_DEF_FILE_NAME=%3
 set PROTO_DEF_FILE_PATH=%SRC_DIR%\%PROTO_DEF_FILE_NAME%
@@ -11,6 +11,6 @@ if exist %DST_DIR%\ rd /q /s %DST_DIR%
 mkdir %DST_DIR%
 
 
-%PROTOBUF_BIN_DIR%\protoc.exe -I=%SRC_DIR% --java_out=%DST_DIR% %PROTO_DEF_FILE_PATH%
+%PROTOBUF_DIR%\bin\protoc.exe -I=%SRC_DIR% --java_out=%DST_DIR% %PROTO_DEF_FILE_PATH%
 
 EXIT
